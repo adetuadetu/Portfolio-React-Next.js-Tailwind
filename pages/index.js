375a518b9f0348a6d26c23f3237eca04e7290928
adetuadetu <jposorio95@hotmail.com>
2023-03-05 20:00:56 +0100

    addition of images and icons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 export default function Home() {
   return (
@@ -13,15 +20,39 @@ export default function Home() {
       <main className='bg-white px-10'>
           <section className='min-h-screen'>
             <nav className='py-10 mb-12 flex justify-between'>
-              <h1 className='text-xl'>Hello</h1>
+              <h1 className='text-xl font-burtons'>Developed By Joan</h1>
               <ul className='flex items-center'>
                 <li>
                   <BsFillMoonStarsFill className='cursor-pointer text-2xl
                   '/>
                 </li>
-                <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+                <li>
+                  <a
+                   className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' 
+                   href='#'
+                   >
+                    Resume
+                  </a>
+                </li>
               </ul>
             </nav>
+            <div className="text-center p-10">
+              <h2 className='text-5xl py-2 text-teal-600 font-medium' >Joan Osorio</h2>
+              <h3 className='text-2xl py-2'>Full-Stack Developer</h3>
+              <p className='text-ms py-5 leading-8 text-gray-800'>
+                iuaefbs asdiufba is siufb iasuf uisaf sifudsi ffsaiu gfsaiuf gbasikjsbcs  sijbsdiu 
+                sfbsdibfs sdiufb sdi usdiu sdiufs dfsi usdius fiufsa iouagio aiufg siofuas iofug aiufga
+                aiou asiufg assdaioufsg fsiudsgoiusag fasifusagsidousgfi as siufg safiosugsa iuosa
+              </p>
+            </div>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+              <AiFillTwitterCircle/>
+              <AiFillLinkedin/>
+              <AiFillYoutube/>
+            </div>
+            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20'>
+              <Image src={deved} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px 50vw, 33vw"/>
+            </div>
             
           </section>
       </main>
