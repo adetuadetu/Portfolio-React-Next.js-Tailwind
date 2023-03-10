b3bdacabc8c88363ca2881ef1fa023925f293065
adetuadetu <jposorio95@hotmail.com>
2023-03-10 16:42:43 +0100

    styling completed

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,12 @@ import deved from "../public/dev-ed-wave.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 export default function Home() {
   return (
@@ -20,14 +26,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-white px-10'>
+      <main className='bg-white px-10 md:px-20 lg:px-40'>
           <section className='min-h-screen'>
             <nav className='py-10 mb-12 flex justify-between'>
               <h1 className='text-xl font-burtons'>Developed By Joan</h1>
               <ul className='flex items-center'>
                 <li>
-                  <BsFillMoonStarsFill className='cursor-pointer text-2xl
-                  '/>
+                  <BsFillMoonStarsFill className='cursor-pointer text-2xl'
+                  />
                 </li>
                 <li>
                   <a
@@ -40,9 +46,9 @@ export default function Home() {
               </ul>
             </nav>
             <div className="text-center p-10">
-              <h2 className='text-5xl py-2 text-teal-600 font-medium' >Joan Osorio</h2>
-              <h3 className='text-2xl py-2'>Full-Stack Developer</h3>
-              <p className='text-ms py-5 leading-8 text-gray-800'>
+              <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Joan Osorio</h2>
+              <h3 className='text-2xl py-2 md:text-3xl'>Full-Stack Developer</h3>
+              <p className='text-ms py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto'>
                 iuaefbs asdiufba is siufb iasuf uisaf sifudsi ffsaiu gfsaiuf gbasikjsbcs  sijbsdiu 
                 sfbsdibfs sdiufb sdi usdiu sdiufs dfsi usdius fiufsa iouagio aiufg siofuas iofug aiufga
                 aiou asiufg assdaioufsg fsiudsgoiusag fasifusagsidousgfi as siufg safiosugsa iuosa
@@ -53,7 +59,7 @@ export default function Home() {
               <AiFillLinkedin/>
               <AiFillYoutube/>
             </div>
-            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
               <Image src={deved} layout="fill" objectFit="cover" />
             </div>
           </section>
@@ -76,8 +82,8 @@ export default function Home() {
                 sdiuv hisuvb asioub vasioub 
               </p>
             </div>
-            <div>
-              <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='lg:flex gap-10'>
+              <div className='grid place-items-center shadow-lg p-10 rounded-xl my-10'>
                 <Image src={design} width={100} height={100} />
                 <h3 className='text-lg font-medium pt-8 pb-2'>
                   Beautiful Designs
@@ -91,7 +97,7 @@ export default function Home() {
                 <p className='text-gray-800 py-1'>Illustrator</p>
                 <p className='text-gray-800 py-1'>Figma</p>
               </div>
-              <div className='text-center items-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='grid place-items-center shadow-lg p-10 rounded-xl my-10'>
                 <Image src={consulting} width={100} height={100} />
                 <h3 className='text-lg font-medium pt-8 pb-2'>
                   Beautiful Designs
@@ -105,7 +111,7 @@ export default function Home() {
                 <p className='text-gray-800 py-1'>Illustrator</p>
                 <p className='text-gray-800 py-1'>Figma</p>
               </div>
-              <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='grid place-items-center shadow-lg p-10 rounded-xl my-10'>
                 <Image src={code} width={100} height={100} object-center/>
                 <h3 className='text-lg font-medium pt-8 pb-2'>
                   Beautiful Designs
@@ -118,6 +124,45 @@ export default function Home() {
                 <p className='text-gray-800 py-1'>Photoshop</p>
                 <p className='text-gray-800 py-1'>Illustrator</p>
                 <p className='text-gray-800 py-1'>Figma</p>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div>
+              <h3 className='text-3xl py-1'>Portfolio</h3>
+              <p className='text-md py-2 leading-8 text-gray-800'>
+                {" "}
+                asoiasnoifn saoif nasoiasf sapiohfdsa poiash pisaof
+                a aopisdah poisadhf oih asdpoifash safiosugsaaspioö fasifusagsidousgfiaposid 
+                dsajklb saijbs okhasndj aspiudb asidfjbsa ijbsadif jsadbiasjdkb sdj
+              <span className='text-teal-500'>agencies</span>
+              consulted for<span className='text-teal-500'>startups</span>
+              ajsdfnisljk askj asdjkfba asjkldfba ifasbj asdjkfbda fijaskb asijb 
+              asp baifjb iabs jdofijsakbijfbsajfbasp f aiufdsb aiusab faui pa 
+              </p>
+              <p className='text-md py-2 leading-8 text-gray-800'>
+                savijbs dsvaiubvsaiiusab sviaub divub isub vsaio sviu basv
+                sdiuv hisuvb asioub vasioub 
+              </p>
+            </div>
+            <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+              <div className='basis-1/3 flex-1'>
+                <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              </div>
+              <div className='basis-1/3 flex-1'>
+                <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              </div>
+              <div className='basis-1/3 flex-1'>
+                <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              </div>
+              <div className='basis-1/3 flex-1'>
+                <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              </div>
+              <div className='basis-1/3 flex-1'>
+                <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              </div>
+              <div className='basis-1/3 flex-1'>
+                <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
               </div>
             </div>
           </section>
